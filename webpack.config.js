5476812272a2f93710ab71a52c8f9ad0c69579d1
volodymyr.matseliukh <volodymyr.matselyukh@yandex.ru>
2023-03-24 02:47:09 +0200

    moving files, adding webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    "mode": "none",
+    "context": path.resolve(__dirname),
+    "entry": {
+        main: "./src/main.js",
+		"bootstrap.min": "./src/bootstrap.min.js",
+		"jquery.min": "./src/jquery.min.js"
+    },
+    "output": {
+        "path": __dirname + '/dist/scripts',
+        "filename": "[name].js"
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+          }
+    },
+    devtool: "source-map",
+	plugins: [
+		new CopyPlugin({
+		  patterns: [
+			{ from: path.resolve(__dirname, "html"), to: path.resolve(__dirname, "dist") }
+		  ],
+		}),
+	  ]
+}
